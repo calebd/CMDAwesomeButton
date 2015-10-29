4590b99e6123151b1a6e03813ba9c98b1c5f365e
Caleb Davenport <caleb@calebd.me>
2015-10-29 11:06:43 -0700

    Tweak configuration and add tvOS target.

---
+++ Tests/Tests.m
@@ -0,0 +1,19 @@
+//
+//  Tests.m
+//  CMDAwesomeButton
+//
+//  Created by Caleb Davenport on 6/28/15.
+//  Copyright (c) 2015 Caleb Davenport. All rights reserved.
+//
+
+@import UIKit;
+@import XCTest;
+@import CMDAwesomeButton;
+
+@interface CMDAwesomeButtonTests : XCTestCase
+
+@end
+
+@implementation CMDAwesomeButtonTests
+
+@end
